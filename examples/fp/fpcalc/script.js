a83f21ccee0238c15e4392bf25ea26f0d3fbe322
s-y-wu <sywu@hmc.edu>
2023-04-06 18:25:43 -0700

    javascript stuffs

--- examples/fp/fpcalc/script.js
+++ examples/fp/fpcalc/script.js
@@ -1,6 +1,15 @@
 let wasm;
 
-WebAssembly.instantiateStreaming(fetch('~/cvw/examples/fp/fpcalc/fpcalc.wasm'))
+const importObject = {
+    env: {
+      memoryBase: 0,
+      tableBase: 0,
+      memory: new WebAssembly.Memory({ initial: 256 }),
+      table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
+    }
+  };
+
+WebAssembly.instantiateStreaming(fetch('http://127.0.0.1:5500/examples/fp/fpcalc/fpcalc.wasm'), importObject)
 .then(obj => {
     wasm = obj.instance.exports;
     console.log('WASM module loaded:', wasm);
@@ -9,3 +18,9 @@ WebAssembly.instantiateStreaming(fetch('~/cvw/examples/fp/fpcalc/fpcalc.wasm'))
     console.error('Failed to load WASM module:', error);
 });
 
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(2, 4);
+console.log('Result:', result);
